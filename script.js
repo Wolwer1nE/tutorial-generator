c75f60c0855ef7dd7184cdfcca83b1aa25c72607
wolwer1ne <wolwerine@yandex.ru>
2019-06-03 11:14:51 +0300

    Output renamed

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    var button = document.createElement("Button");
+    button.className="btn btn-success tutorial";
+    button.innerHTML = "?";
+    button.id = "startTutorial";
+    button.style = "top:0;right:0;position:absolute;z-index: 9999";
+    document.body.appendChild(button);
+
+    $('label:contains("Название")').siblings("input, select ").attr("data-intro", " Название модели должно быть корректным именем файла и быть уникальным.").attr("data-step", 1);
+$('label:contains("Элементов на оси")').siblings("input, select ").attr("data-intro", " Представительные объёмы в ACELAN генерируются на основе octree.").attr("data-step", 2);
+$('label:contains("Тип связности")').siblings("input, select ").attr("data-intro", " Тип связности и алгоритм генерации определяют список входных параметров.").attr("data-step", 3);
+$('label:contains("% включений")').siblings("input, select ").attr("data-intro", " Для большинства генераторов % включений не может быть менее 5%.").attr("data-step", 4);
+
+});
+
+$(document).on("click", "button.tutorial" , function() {
+
+    introJs().start();
+
+});
